f12817f08fc03c831d3f1202c36a0a0d2a8460d0
DeenLeibovici <deenleibovici@ufl.edu>
2025-02-04 07:08:49 +0800

    Library Linking Works

---
+++ build/CMakeFiles/spinnaker_example.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spinnaker_example.dir/main.cpp.o"
+  "CMakeFiles/spinnaker_example.dir/FLIRCamera.cpp.o"
+  "CMakeFiles/spinnaker_example.dir/GPU.cpp.o"
+  "spinnaker_example.pdb"
+  "spinnaker_example"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spinnaker_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
